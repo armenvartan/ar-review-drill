9adbda0e66b874e5ac1d34f96ec4d2d59836214e
Stephen Estrada <stcrestrada@gmail.com>
2014-08-19 15:59:01 -0400

    setup models and migrations

--- hospital.rb
+++ hospital.rb
@@ -5,10 +5,25 @@ ActiveRecord::Base.establish_connection adapter: "sqlite3", database: ":memory:"
 # Define your migrations below:
 # for example:
 #
-# ActiveRecord::Migration.create_table :fruits do |t|
-#   t.string :name
-#   t.belongs_to :bowl
-# end
+ActiveRecord::Migration.create_table :doctors do |t|
+  t.string :name
+  t.belongs_to :bowl
+end
+
+ActiveRecord::Migration.create_table :users do |t|
+  t.string :name
+  t.belongs_to :bowl
+end
+
+ActiveRecord::Migration.create_table :appointments do |t|
+  t.string :name
+  t.belongs_to :bowl
+end
+
+ActiveRecord::Migration.create_table :symptoms do |t|
+  t.string :name
+  t.belongs_to :bowl
+end
 #
 # Repeat the above template for each table you need to create
 #
@@ -21,9 +36,22 @@ ActiveRecord::Migrator.up "db/migrate"
 # Define your AR classes below:
 # for example:
 #
-# class Fruit < ActiveRecord::Base
-#   belongs_to :bowl
-# end
+class Doctor < ActiveRecord::Base
+  
+end
+
+class User < ActiveRecord::Base
+  
+end
+
+class Appointment < ActiveRecord::Base
+  
+end
+
+class Symptom < ActiveRecord::Base
+  
+end
+
 #
 # You can define multiple classes, one after another
 
