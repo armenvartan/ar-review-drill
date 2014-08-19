0263b0ac3dbeb6ce79e53eb74ee358d3f10736bf
Stephen Estrada <stcrestrada@gmail.com>
2014-08-19 16:42:12 -0400

    added migrations datatypes

--- source/db/migrate/20140819163537_create_appointments.rb
+++ source/db/migrate/20140819163537_create_appointments.rb
@@ -1,4 +1,10 @@
 class CreateAppointments < ActiveRecord::Migration
   def change
+  	create_table :appointments do |t|
+  		t.references :doctor
+  		t.references :user
+
+  		t.timestamps
+  	end
   end
 end
