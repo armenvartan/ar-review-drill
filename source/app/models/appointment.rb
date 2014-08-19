18a25fe46b00c47930bc6eac7b4ddd03e8c1a91d
Stephen Estrada <stcrestrada@gmail.com>
2014-08-19 16:46:02 -0400

    added associations

--- source/app/models/appointment.rb
+++ source/app/models/appointment.rb
@@ -1,3 +1,4 @@
 class Appointment < ActiveRecord::Base
-	
+	belongs_to :patient, class_name: 'User', foreign_key: :user_id
+	belongs_to :doctor
 end
--- source/app/models/symptom.rb
+++ source/app/models/symptom.rb
@@ -1,3 +1,4 @@
 class Symptom < ActiveRecord::Base
+	belongs_to :patient, class_name: 'User', foreign_key: :user_id
 	
 end
